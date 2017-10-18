5950cc162c591d3a78a73da37f9234dfb55460e7
Hajbok Robert <hajbok.robert@gmail.com>
2017-10-18 16:55:54 +0100

    Initial commit, project setup

--- AngularPWA/Startup.cs
+++ AngularPWA/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace AngularPWA
 {
@@ -15,20 +12,31 @@ namespace AngularPWA
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            // Redirect any non-API calls to the Angular application so our application can handle the routing
+            app.Use(async (context, next) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                   !Path.HasExtension(context.Request.Path.Value) &&
+                   !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
             });
+
+            // Configures application for usage as API with default route of '/api/[Controller]'
+            app.UseMvcWithDefaultRoute();
+
+            // Configures application to serve the index.html file from /wwwroot when you access the server from a web browser
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
