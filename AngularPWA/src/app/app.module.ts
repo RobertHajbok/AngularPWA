c65416477136718c39d67dcd56291bf325b1e052
Robert Hajbok <hajbok.robert@gmail.com>
2020-12-30 04:03:15 +0000

    Upgrade Angular from v4 to v9

--- AngularPWA/src/app/app.module.ts
+++ AngularPWA/src/app/app.module.ts
@@ -1,50 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { GeolocationService } from "./geolocation.service";
 import { DataService } from "./data.service";
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule, MatToolbarModule, MatCardModule, MatSlideToggleModule, MatSnackBarModule } from '@angular/material';
-import 'hammerjs';
 import { ListComponent } from './list/list.component';
 import { CoffeeComponent } from './coffee/coffee.component';
-import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from '../environments/environment';
 
 const routes: Routes = [
-    { path: '', component: ListComponent },
-    { path: 'coffee', component: CoffeeComponent },
-    { path: 'coffee/:id', component: CoffeeComponent }
+  { path: '', component: ListComponent },
+  { path: 'coffee', component: CoffeeComponent },
+  { path: 'coffee/:id', component: CoffeeComponent }
 ]
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ListComponent,
-        CoffeeComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        HttpModule,
-        MatButtonModule,
-        MatIconModule,
-        MatInputModule,
-        MatSelectModule,
-        MatSliderModule,
-        MatToolbarModule,
-        MatCardModule,
-        MatSlideToggleModule,
-        MatSnackBarModule,
-        RouterModule.forRoot(routes),
-        ServiceWorkerModule
-    ],
-    providers: [GeolocationService, DataService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ListComponent,
+    CoffeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    RouterModule.forRoot(routes),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [GeolocationService, DataService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- AngularPWA/src/app/data.service.ts
+++ AngularPWA/src/app/data.service.ts
@@ -1,42 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Coffee } from './logic/coffee';
 import { PlaceLocation } from './logic/place-location';
-import { Http } from '@angular/http';
 
 @Injectable()
 export class DataService {
+  constructor(private http: HttpClient) { }
 
-    constructor(private http: Http) { }
+  get(coffeeId: number, callback) {
+    this.http.get(`/api/coffees/${coffeeId}`).subscribe(response => {
+      callback(response);
+    });
+  }
 
-    get(coffeeId: number, callback) {
-        this.http.get(`/api/coffees/${coffeeId}`).subscribe(response => {
-            callback(response.json());
-        });
+  getList(callback) {
+    //const list = [
+    //    new Coffee('Double Espresso', 'Sunny Cafe', new PlaceLocation('123 Market St', 'San Francisco')),
+    //    new Coffee('Caramel Americano', 'Starcoffee', new PlaceLocation('Gran Via 34', 'Madrid'))
+    //];
+    //callback(list);
+    this.http.get('/api/coffees').subscribe(response => {
+      console.log(response);
+      callback(response);
+    });
+  }
+
+  save(coffee, callback) {
+    if (coffee.id) {
+      // It's an update
+      this.http.put(`/api/coffees/${coffee.id}`, coffee, { responseType: 'text' }).subscribe(response => {
+        callback(true);
+      });
+    } else {
+      // It's an insert
+      this.http.post('/api/coffees', coffee, { responseType: 'text' }).subscribe(response => {
+        callback(true);
+      });
     }
-
-    getList(callback) {
-        //const list = [
-        //    new Coffee('Double Espresso', 'Sunny Cafe', new PlaceLocation('123 Market St', 'San Francisco')),
-        //    new Coffee('Caramel Americano', 'Starcoffee', new PlaceLocation('Gran Via 34', 'Madrid'))
-        //];
-        //callback(list);
-        this.http.get('/api/coffees').subscribe(response => {
-            console.log(response.json());
-            callback(response.json());
-        });
-    }
-
-    save(coffee, callback) {
-        if (coffee.id) {
-            // It's an update
-            this.http.put(`/api/coffees/${coffee.id}`, coffee).subscribe(response => {
-                callback(true);
-            });
-        } else {
-            // It's an insert
-            this.http.post('/api/coffees', coffee).subscribe(response => {
-                callback(true);
-            });
-        }
-    }
+  }
 }
